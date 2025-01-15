fbe24604f58b7200cf15a40912982a1ba97c33ca
xiaoyang9730 <xiaoyang9730@outlook.com>
2025-01-15 21:22:53 +0800

    2024 day 2 part 2 passed

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -2,26 +2,49 @@ use std::env::args;
 use std::fs::File;
 use std::io::read_to_string;
 
-fn part_1(input: String) -> u32 {
-    input.lines()
-        .filter(|line| {
-            let levels: Vec<u32> = line.split(' ').map(|s| s.parse().unwrap()).collect();
-            let initial_dir = levels[0].ge(&levels[1]);
-            for w in levels.windows(2) {
-                let current_dir = w[0].ge(&w[1]);
-                let diff = w[0].abs_diff(w[1]);
-                if initial_dir != current_dir || diff < 1 || diff > 3 {
-                    return false;
+fn parse(input: String) -> Vec<Vec<u32>> {
+    input.lines().map(|l| l.split(' ').map(|s| s.parse().unwrap()).collect()).collect()
+}
+
+fn predicate(levels: &&Vec<u32>) -> bool {
+    let initial_dir = levels[0].ge(&levels[1]);
+    for w in levels.windows(2) {
+        let current_dir = w[0].ge(&w[1]);
+        let diff = w[0].abs_diff(w[1]);
+        if initial_dir != current_dir || diff < 1 || diff > 3 {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn part_1(reports: &Vec<Vec<u32>>) -> usize {
+    reports.iter().filter(predicate).count()
+}
+
+fn part_2(reports: &Vec<Vec<u32>>) -> usize {
+    reports.iter()
+        .filter(|levels| {
+            if predicate(&&levels) {
+                return true;
+            }
+            for i in 0..levels.len() {
+                let mut cloned = (*levels).clone();
+                cloned.remove(i);
+                if predicate(&&cloned) {
+                    return true;
                 }
             }
-            return true;
+            return false;
         })
-        .count() as _
+        .count()
 }
 
 fn main() {
     let filename = args().skip(1).next().unwrap();
     let input = read_to_string(File::open(filename).unwrap()).unwrap();
 
-    println!("part 1: {}", part_1(input));
+    let reports = parse(input);
+    println!("part 1: {}", part_1(&reports));
+    println!("part 2: {}", part_2(&reports));
 }
