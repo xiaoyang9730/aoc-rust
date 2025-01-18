6a2167004356a48c692208e611adbbb35eb1bec5
xiaoyang9730 <xiaoyang9730@outlook.com>
2025-01-18 09:16:01 +0800

    2024 day 7 part 2 passed

--- 2024/day7/src/main.rs
+++ 2024/day7/src/main.rs
@@ -37,10 +37,40 @@ fn part_1(equations: &Vec<Equation>) -> u64 {
         .fold(0, |acc, v| acc + v)
 }
 
+fn part_2(equations: &Vec<Equation>) -> u64 {
+    equations.iter()
+        .filter_map(|(target, numbers)| {
+            for mut op in 0..3u32.pow(numbers.len() as u32 - 1) {
+                let mut result = numbers[0];
+                for i in 1..numbers.len() {
+                    match op % 3 {
+                        0 => result += numbers[i],
+                        1 => result *= numbers[i],
+                        _ => {
+                            let mut number = numbers[i];
+                            while number != 0 {
+                                result *= 10;
+                                number /= 10;
+                            }
+                            result += numbers[i];
+                        },
+                    }
+                    op /= 3;
+                }
+                if result == *target {
+                    return Some(*target);
+                }
+            }
+            return None;
+        })
+        .fold(0, |acc, v| acc + v)
+}
+
 fn main() {
     let filename = args().skip(1).next().unwrap();
     let input = read_to_string(File::open(filename).unwrap()).unwrap();
 
     let equations = parse(input);
     println!("part 1: {}", part_1(&equations));
+    println!("part 2: {}", part_2(&equations));
 }
