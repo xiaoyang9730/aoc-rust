35f84c4fb5069c8052dbf85ed2df67a4beb30150
xiaoyang9730 <xiaoyang9730@outlook.com>
2025-01-23 21:50:37 +0800

    add progress tracker for lut initializaton

--- 2024/day11/src/lut.rs
+++ 2024/day11/src/lut.rs
@@ -37,8 +37,32 @@ impl Lut {
 
     #[inline]
     fn table(step: usize, stone_range: Range<usize>) -> Vec<Stones> {
+        let mut progress = LutInitProgress::new(stone_range.clone(), 8);
         stone_range
-            .map(|stone| utils::repeat(blink::all, vec![stone], step))
+            .map(|stone| {
+                progress.check(stone);
+                utils::repeat(blink::all, vec![stone], step)
+            })
             .collect()
+    }
+}
+
+struct LutInitProgress {
+    milestone: usize,
+    phase_len: usize,
+    range: Range<usize>,
+}
+
+impl LutInitProgress {
+    fn new(range: Range<usize>, milestones: usize) -> Self {
+        let phase_len = range.clone().count() / milestones;
+        Self { milestone: range.start, phase_len, range }
+    }
+
+    fn check(&mut self, progress: usize) {
+        if progress >= self.milestone {
+            println!("Progress...{:>3.1}%", (self.milestone - self.range.start) as f32 / self.range.clone().count() as f32 * 100.0);
+            self.milestone += self.phase_len;
+        }
     }
 }
